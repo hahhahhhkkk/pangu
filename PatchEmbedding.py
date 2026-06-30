60c1870d5d19d34e2de71c2051a144914e061b0d
Ashuo <120069395+hahhahhhkkk@users.noreply.github.com>
2026-06-30 16:26:55 +0800

    Update PatchEmbedding.py

--- PatchEmbedding.py
+++ PatchEmbedding.py
@@ -1,22 +1,28 @@
 import torch
 # 在数据输入到网络的准备 对hw减半和对zhw减半 并将特征维度映射到dim
 # 地面数据也算是一个高度层 最后在高度层进行拼接
-class PatchEmbedding(torch.nn.Module):
-    def __init__(self, up_channel=5, down_channel=7, out_channel=192, patch=(2, 2, 2), stride=(2, 2, 2)):
-        super().__init__()
-        self.c3d = torch.nn.Conv3d(in_channels=up_channel, out_channels=out_channel, kernel_size=patch, stride=stride)
-        self.c2d = torch.nn.Conv2d(in_channels=down_channel, out_channels=out_channel, kernel_size=patch[1:],
-                                   stride=stride[1:])
+class PatchEmbedding:
+    def __init__(self, patch_size, dim):
+        self.conv = Conv3d(5, dim, kernel_size=patch_size, stride=patch_size)
+        self.conv_surface = Conv2d(7, dim, kernel_size=patch_size[1:], stride=patch_size[1:])
 
-    def forward(self, up, down):
-        # up = F.pad(up, (0, 0, 1, 0, 0, 0), mode='constant', value=0) 数据是奇数进行pad
-        # down = F.pad(down, (1, 1, 1, 1), mode='constant', value=0)
 
-        up = self.c3d(up)
-        down = self.c2d(down)
-        down = down.unsqueeze(2)  # 增加维度 匹配高度特征
+        self.var_scale = nn.Parameter(torch.ones(5))
 
-        data = torch.cat([down, up], axis=2)
-        data = data.permute(0, 2, 3, 4, 1)
-        data = data.reshape(data.shape[0], data.shape[1] * data.shape[2] * data.shape[3], -1)  # 做成Transformer形式
-        return data
+    def forward(self, x, surface):
+
+
+        x = x * self.var_scale[None, :, None, None, None]
+
+        x = self.conv(x)
+
+        surface = self.conv_surface(surface)
+
+        x = Concatenate(x, surface)
+
+        x = x * self.lat_weight[None, None, :, None]
+
+        x = x.permute(0, 2, 3, 4, 1)
+        x = x.reshape(x.shape[0], -1, x.shape[-1])
+
+        return x
